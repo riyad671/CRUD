60491c1bf4bd51f39d75ce311894ba3e3f98f30c
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-08-31 21:42:54 +0600

    Edit welcome.blade.php file and add title option

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Bootstrap Example</title>
+    <title>CRUD</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
@@ -13,23 +13,17 @@
 <body>
 
 <div class="container">
-    <h2>Stacked form</h2>
-    <form action="/action_page.php">
-        <div class="form-group">
-        <label for="email">Email:</label>
-        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+    <div class="row justify-content-center">
+        <div class="col-sm-6 pt-2">
+            <form action="/action_page.php">
+                <div class="form-group">
+                {{-- <label for="title">Title:</label> --}}
+                <input type="title" class="form-control" id="title" placeholder="Enter title" name="title">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
         </div>
-        <div class="form-group">
-        <label for="pwd">Password:</label>
-        <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
-        </div>
-        <div class="form-group form-check">
-        <label class="form-check-label">
-            <input class="form-check-input" type="checkbox" name="remember"> Remember me
-        </label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    </div>
 </div>
 
 </body>
